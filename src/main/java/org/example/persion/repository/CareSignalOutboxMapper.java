e99360319f784dfee02806ab413b40fd02f17437
dream <180424490@qq.com>
2026-08-29 02:19:09 +0800

    feat(care): add signal outbox mapper

---
+++ src/main/java/org/example/persion/repository/CareSignalOutboxMapper.java
@@ -0,0 +1,9 @@
+package org.example.persion.repository;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.example.persion.entity.CareSignalOutbox;
+
+@Mapper
+public interface CareSignalOutboxMapper extends BaseMapper<CareSignalOutbox> {
+}

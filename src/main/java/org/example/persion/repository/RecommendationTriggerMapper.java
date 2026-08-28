f2632e6a8c8c418e8ebb38ce952057e5d4dce09a
dream <180424490@qq.com>
2026-08-29 01:54:50 +0800

    feat(recommendation): add proactive trigger mapper

---
+++ src/main/java/org/example/persion/repository/RecommendationTriggerMapper.java
@@ -0,0 +1,9 @@
+package org.example.persion.repository;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.example.persion.entity.RecommendationTrigger;
+
+@Mapper
+public interface RecommendationTriggerMapper extends BaseMapper<RecommendationTrigger> {
+}
